fd3c6a8bc377057f5ac24d34e1e8e2dd3f2529e5
Larry Banks <larrykbanks21@gmail.com>
2025-08-15 10:36:52 -0400

    refactor: serve metrics over http instead of periodically pushing

--- monitoring/agent/agent.py
+++ monitoring/agent/agent.py
@@ -1,9 +1,7 @@
 import logging
-import os
-import time
-import sys
 
 import psutil
+from bottle import route, run
 
 import monitoring.config as config
 
@@ -13,35 +11,32 @@ logger = logging.getLogger(__name__)
 logger.setLevel(config.LOG_LEVEL)
 
 """
-Agent collects metrics and reports to collector
+Tool for collecting and serving metrics over http
 """
 
 
 def collect_metrics():
     metrics = {
-        "cpu": {"cpu_usage": psutil.cpu_percent(), "cpu_freq": psutil.cpu_freq()},
-        "mem": {"phys": psutil.virtual_memory(), "swap": psutil.swap_memory()},
+        "cpu": {
+            "cpu_usage": psutil.cpu_percent(),
+            "cpu_freq": psutil.cpu_freq()._asdict(),
+        },
+        "mem": {
+            "phys": psutil.virtual_memory()._asdict(),
+            "swap": psutil.swap_memory()._asdict(),
+        },
         "disk": [
-            psutil.disk_usage(part.mountpoint) for part in psutil.disk_partitions()
+            psutil.disk_usage(part.mountpoint)._asdict()
+            for part in psutil.disk_partitions()
         ],
     }
     return metrics
 
 
-def run():
-    while True:
-        print(collect_metrics())
-        time.sleep(10)
+@route("/collect")
+def index():
+    return collect_metrics()
 
 
 if __name__ == "__main__":
-    logger.info("Started agent")
-    try:
-        run()
-    except KeyboardInterrupt:
-        logger.info("Interrupted")
-        try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
-    logger.info("Shutdown agent")
+    run()
